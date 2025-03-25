ca056dd86783f7a91ae4db5017a2945478fda372
haewook Lee <atolow@gmail.com>
2025-03-26 02:56:21 +0900

    feat: add package

--- src/main/java/example/com/gameserver/GameserverApplication.java
+++ src/main/java/example/com/gameserver/GameserverApplication.java
@@ -2,7 +2,9 @@ package example.com.gameserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class GameserverApplication {
 
