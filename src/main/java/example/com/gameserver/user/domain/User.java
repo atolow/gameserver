bde75f8e240b5624224656e3a796c100e3e13792
haewook Lee <atolow@gmail.com>
2025-03-26 02:56:21 +0900

    feat: add package

--- src/main/java/example/com/gameserver/user/domain/User.java
+++ src/main/java/example/com/gameserver/user/domain/User.java
@@ -1,4 +1,39 @@
 package example.com.gameserver.user.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true, length = 15)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private int balance;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private boolean isActive = true;
 }
